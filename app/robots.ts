4f5e0b6fa9731e1a4cdb599586176c40fa76dcb1
kirankumariq201 <kirankumar1116t@gmail.com>
2026-09-15 20:01:19 +0530

    Create app/robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://anits-faculty-4zw6.vercel.app/sitemap.xml",
+  };
+}
